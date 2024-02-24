dc829cae059a198da9a5f34f0105eb19ee8e7ca1
Kadeall <abdishakuradan4040@gmail.com>
2024-02-24 14:04:42 +0000

    s

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+const hamburger_menu = document.querySelector(".hamburger_menu")
+const nav_menu = document.querySelector(".nav_menu")
+
+hamburger_menu.addEventListener("click", () => {
+    hamburger_menu.classList.toggle("active");
+    nav_menu.classList.toggle("active");
 }
+)
+document.querySelectorAll(".nav_link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger_menu.classList.remove("active");
+        nav_menu.classList.remove("active");
+    }))
